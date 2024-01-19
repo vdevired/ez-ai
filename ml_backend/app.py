0542a3b7bb68e782bff925bdf406ab08357c4d7a
Vikhyat <vdevired@usc.edu>
2024-01-18 22:57:49 -0800

    Add util functions to list out files in dataset and read image bytes

--- ml_backend/app.py
+++ ml_backend/app.py
@@ -17,3 +17,14 @@ def upload():
     source_path = "tmp/dummy.keras"
     s3_destination_path = "models/01/dummy.keras"
     return utils.upload_to_s3(app, source_path, s3_destination_path)
+
+@app.route("/get_image")
+def get_image():
+    s3_source_path = "datasets/test/daisy/10172379554_b296050f82_n.jpg"
+    img = utils.fetch_image(app, s3_source_path)
+    return "<p>Success!</p>"
+
+@app.route("/list")
+def list_out():
+    res = utils.list_files_in_dataset(app, "test")
+    return '\n'.join(res)
--- ml_backend/utils.py
+++ ml_backend/utils.py
@@ -1,4 +1,7 @@
 import boto3
+import io
+from keras.preprocessing.image import load_img
+
 
 def upload_to_s3(app, source_path, s3_destination_path):
     """
@@ -16,3 +19,40 @@ def upload_to_s3(app, source_path, s3_destination_path):
     except (ClientError, FileNotFoundError) as e:
         print(e)
         return "Upload Failed"
+
+def list_files_in_dataset(app, dataset_id):
+    """
+    app: The flask app, has s3 config store in app.config
+    source_path: relative to py file
+    dataset_id: The ID of the dataset
+
+    returns list of strings of format 'label/filename'
+    """
+    s3_client = boto3.client('s3', aws_access_key_id=app.config['S3_KEY'],
+                             aws_secret_access_key=app.config['S3_SECRET'])
+    res = []
+
+    prefix = f'datasets/{dataset_id}/'
+    paginator = s3_client.get_paginator('list_objects')
+    operation_parameters = {'Bucket': app.config['S3_BUCKET'],
+                            'Prefix': prefix}
+    page_iterator = paginator.paginate(**operation_parameters)
+    for page in page_iterator:
+        for entry in page['Contents']:
+            res.append(entry['Key'].replace(prefix, ""))
+
+    return res
+
+def fetch_image(app, s3_source_path):
+    """
+    app: The flask app, has s3 config store in app.config
+    source_path: relative to py file
+    s3_source_path: The path of the image in s3
+    """
+    s3_client = boto3.client('s3', aws_access_key_id=app.config['S3_KEY'],
+                             aws_secret_access_key=app.config['S3_SECRET'])
+
+    response = s3_client.get_object(Bucket=app.config['S3_BUCKET'], Key=s3_source_path)
+    img = load_img(io.BytesIO(response['Body'].read()))
+    # img.show()
+    return img
